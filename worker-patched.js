e7ab5874390e290a0cc973ce107f77eb35eb6dda
GTD-TFS <adeje.gtd@hotmail.com>
2025-10-04 12:49:08 +0100

    Create worker-patched.js

---
+++ worker-patched.js
@@ -0,0 +1,10 @@
+// worker-patched.js
+const wasmURL = location.origin + location.pathname + 'tesseract-core-simd.wasm';
+const origFetch = self.fetch.bind(self);
+self.fetch = async function(r, i) {
+  if (typeof r === 'string' && r.endsWith('tesseract-core-simd.wasm')) {
+    return origFetch(wasmURL, i);
+  }
+  return origFetch(r, i);
+};
+importScripts('worker.min.js');
